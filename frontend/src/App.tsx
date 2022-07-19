2af602152e95f623cdb561c0687edde3630e547b
JacobMcElhinney <jacobmcelhinney@gmail.com>
2022-07-19 04:03:19 +0300

    Add dynamic routing and store provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,58 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import ReadPostPage from "./features/posts/ReadPostPage";
+import PostIndexPage from "./features/posts/PostIndexPage";
+import Layout from "./components/Layout";
+import CreateCommentPage from "./features/comments/CreateCommentPage";
+import CreatePostPage from "./features/posts/CreatePostPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+
+          <Route index element={<PostIndexPage />} /> {/* http://localhost:3000/ */}
+          
+          <Route path="post">
+            <Route index element={<CreatePostPage />} /> {/* http://localhost:3001/post */}
+            <Route path=":postId" element={<ReadPostPage />} /> {/* http://localhost:3001/post/1 */}
+            <Route path=":postId/comment" element={<CreateCommentPage />} /> {/* http://localhost:3001/post/1/comment */}
+          </Route>
+ 
+          <Route path="*" element={<Navigate to="/" replace />} /> 
+
+        </Route>
+      </Routes>
   );
 }
 
-export default App;
+export default App; 
