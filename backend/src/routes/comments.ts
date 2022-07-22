bf3b6639cf60c7df4b12a592ea60a026d6b5f911
JacobMcElhinney <jacobmcelhinney@gmail.com>
2022-07-22 12:43:17 +0300

    Delete test action method

--- backend/src/routes/comments.ts
+++ backend/src/routes/comments.ts
@@ -2,17 +2,17 @@ import express from "express";
 import {
   getAllComments,
   getCommentById,
-  getCommentsByPostId,
   createComment,
   updateComment,
+  deleteComment,
 } from "../controllers/commentController";
 
 const commentsRouter = express.Router();
 
 commentsRouter.get("/", getAllComments);
 commentsRouter.get("/:id", getCommentById);
-commentsRouter.get("/post/:id", getCommentsByPostId); //get post and all comments associated with it
 commentsRouter.post("/", createComment);
 commentsRouter.put("/:id", updateComment);
+commentsRouter.delete("/:id", deleteComment);
 
 export default commentsRouter;
